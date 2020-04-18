4eae090c97d1dfa4453c7d88cafba2581420efcc
Shengxuan Zhao <shengxuanzhao97@gmail.com>
2020-04-18 11:32:44 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace SearchFile
+{
+    partial class TextSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FileNameLable = new System.Windows.Forms.Label();
+            this.SearchforLable = new System.Windows.Forms.Label();
+            this.FileNameTextBox = new System.Windows.Forms.TextBox();
+            this.SearchforTextBox = new System.Windows.Forms.TextBox();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.ResultListView = new System.Windows.Forms.ListView();
+            this.LineNoColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.TextColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.StatusBar = new System.Windows.Forms.StatusStrip();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.FCStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.ProgressBar = new System.Windows.Forms.ProgressBar();
+            this.ResultNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.StatusBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // FileNameLable
+            // 
+            this.FileNameLable.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FileNameLable.AutoSize = true;
+            this.FileNameLable.Location = new System.Drawing.Point(25, 19);
+            this.FileNameLable.Name = "FileNameLable";
+            this.FileNameLable.Size = new System.Drawing.Size(54, 13);
+            this.FileNameLable.TabIndex = 0;
+            this.FileNameLable.Text = "File Name";
+            // 
+            // SearchforLable
+            // 
+            this.SearchforLable.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SearchforLable.AutoSize = true;
+            this.SearchforLable.Location = new System.Drawing.Point(25, 51);
+            this.SearchforLable.Name = "SearchforLable";
+            this.SearchforLable.Size = new System.Drawing.Size(56, 13);
+            this.SearchforLable.TabIndex = 1;
+            this.SearchforLable.Text = "Search for";
+            // 
+            // FileNameTextBox
+            // 
+            this.FileNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FileNameTextBox.Location = new System.Drawing.Point(88, 19);
+            this.FileNameTextBox.Name = "FileNameTextBox";
+            this.FileNameTextBox.Size = new System.Drawing.Size(537, 20);
+            this.FileNameTextBox.TabIndex = 2;
+            this.FileNameTextBox.TextChanged += new System.EventHandler(this.SetSearchButtonEnable);
+            // 
+            // SearchforTextBox
+            // 
+            this.SearchforTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SearchforTextBox.Location = new System.Drawing.Point(88, 51);
+            this.SearchforTextBox.Name = "SearchforTextBox";
+            this.SearchforTextBox.Size = new System.Drawing.Size(537, 20);
+            this.SearchforTextBox.TabIndex = 3;
+            this.SearchforTextBox.TextChanged += new System.EventHandler(this.SetSearchButtonEnable);
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BrowseButton.Location = new System.Drawing.Point(661, 15);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.BrowseButton.TabIndex = 4;
+            this.BrowseButton.Text = "Browse";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.BrowseOnClick);
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SearchButton.Location = new System.Drawing.Point(661, 51);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 5;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchOnClick);
+            // 
+            // ResultListView
+            // 
+            this.ResultListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ResultListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ResultNumber,
+            this.LineNoColumn,
+            this.TextColumn});
+            this.ResultListView.HideSelection = false;
+            this.ResultListView.Location = new System.Drawing.Point(28, 91);
+            this.ResultListView.Name = "ResultListView";
+            this.ResultListView.Size = new System.Drawing.Size(708, 291);
+            this.ResultListView.TabIndex = 6;
+            this.ResultListView.UseCompatibleStateImageBehavior = false;
+            this.ResultListView.View = System.Windows.Forms.View.Details;
+            // 
+            // LineNoColumn
+            // 
+            this.LineNoColumn.Text = "Line NO.";
+            // 
+            // TextColumn
+            // 
+            this.TextColumn.Text = "Text";
+            this.TextColumn.Width = 550;
+            // 
+            // StatusBar
+            // 
+            this.StatusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel,
+            this.FCStatusLabel,
+            this.toolStripStatusLabel});
+            this.StatusBar.Location = new System.Drawing.Point(0, 439);
+            this.StatusBar.MinimumSize = new System.Drawing.Size(800, 0);
+            this.StatusBar.Name = "StatusBar";
+            this.StatusBar.Size = new System.Drawing.Size(800, 22);
+            this.StatusBar.TabIndex = 7;
+            this.StatusBar.Text = "statusStrip";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(45, 17);
+            this.StatusLabel.Text = "Status: ";
+            // 
+            // FCStatusLabel
+            // 
+            this.FCStatusLabel.Name = "FCStatusLabel";
+            this.FCStatusLabel.Size = new System.Drawing.Size(42, 17);
+            this.FCStatusLabel.Text = "Ready.";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // ProgressBar
+            // 
+            this.ProgressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ProgressBar.Location = new System.Drawing.Point(28, 398);
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.Size = new System.Drawing.Size(708, 23);
+            this.ProgressBar.TabIndex = 8;
+            // 
+            // ResultNumber
+            // 
+            this.ResultNumber.Text = "Result NO.";
+            this.ResultNumber.Width = 70;
+            // 
+            // TextSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.ProgressBar);
+            this.Controls.Add(this.StatusBar);
+            this.Controls.Add(this.ResultListView);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.SearchforTextBox);
+            this.Controls.Add(this.FileNameTextBox);
+            this.Controls.Add(this.SearchforLable);
+            this.Controls.Add(this.FileNameLable);
+            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.Name = "TextSearch";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Text Search";
+            this.Load += new System.EventHandler(this.TextSearch_Load);
+            this.StatusBar.ResumeLayout(false);
+            this.StatusBar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label FileNameLable;
+        private System.Windows.Forms.Label SearchforLable;
+        private System.Windows.Forms.TextBox FileNameTextBox;
+        private System.Windows.Forms.TextBox SearchforTextBox;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.ListView ResultListView;
+        private System.Windows.Forms.ColumnHeader LineNoColumn;
+        private System.Windows.Forms.ColumnHeader TextColumn;
+        private System.Windows.Forms.StatusStrip StatusBar;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.ProgressBar ProgressBar;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ToolStripStatusLabel FCStatusLabel;
+        private System.Windows.Forms.ColumnHeader ResultNumber;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,198 @@
+﻿/*
+ * This program is used for searching a key word in a selected txt file
+ * Case-insensitive, key word part contained count(like search "good" also can get "goodness")
+ * 
+ * Using step: 
+ * 1. Click "Browse" button to select a txt file; 
+ * 2. input a key word; 
+ * 3. If "Search" button is clickable, clikc it to start searching; 
+ * 4. Searching result will be showed in below list view.
+ * 5. If "Search" button is not clickable, please check whether file path is valid and key word is not empty
+ * 6. "Search" will turn to "Cancel" once searching process begin, click "Cancel" the searching process will stop at once
+ *
+ * The process bar will give users a approximate sence of how long they still have to wait
+ * The status bar will show program's status and how many result already found
+ * ListView show the result with result line number and content, and will focus on the newest line
+ */
+
+using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SearchFile
+{
+    
+    public partial class TextSearch : Form
+    {
+        private BackgroundWorker myBackgroundWorker = new BackgroundWorker();
+        private String FileName;
+        private String KeyWord;
+        private int findNumber = 0;
+        private int count = 0;
+        private int LineNumber = 0;
+        private Boolean Cancelled = false;
+        private int totalline = 20000; // hard code the total task amount to avoid read the file twice, may not precise especially for small files
+
+        private void BrowseOnClick(object sender, EventArgs e)
+        {
+            // Show the open file dialog
+            // Save th eseelcted file's path as FileName and show it in the textbox
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                FileName = openFileDialog.FileName;
+                FileNameTextBox.Text = FileName;
+            }
+        }
+
+        // once the text in filename textbox or keyword textbox is changed, check validation
+        // make "Search" button not clickable if file not exist or key word is empty
+        private void SetSearchButtonEnable(object sender, EventArgs e)
+        {
+            KeyWord = SearchforTextBox.Text;           
+            if (KeyWord == "" || KeyWord == null || !File.Exists(FileNameTextBox.Text))
+            {
+                SearchButton.Enabled = false;
+            }
+            else
+            {
+                SearchButton.Enabled = true;
+            }
+        }
+
+        // "Search" button, also "Cancel" button, change when it is clicked
+        // program status and Cancelled status also changed when this button is clicked
+        private void SearchOnClick(object sender, EventArgs e)
+        {
+            KeyWord = SearchforTextBox.Text;
+            if (SearchButton.Text == "Cancel")
+            {
+                FileNameTextBox.ReadOnly = false;
+                SearchforTextBox.ReadOnly = false;
+                toolStripStatusLabel.Text = "Searching precess is cancellend.";
+                StatusBar.Refresh();
+                SearchButton.Text = "Search";
+                Cancelled = true;
+            }
+            else if(SearchButton.Text == "Search")
+            {
+                FileNameTextBox.ReadOnly = true;
+                SearchforTextBox.ReadOnly = true;
+                toolStripStatusLabel.Text = "Searching precess start.";
+                StatusBar.Refresh();
+                ResultListView.Items.Clear();
+                ResultListView.Update();
+                ResultListView.Refresh();
+                ProgressBar.Value = 0;
+                SearchButton.Text = "Cancel";
+                Cancelled = false;
+            }
+            
+            if(!Cancelled)
+            {
+                myBackgroundWorker.RunWorkerAsync();
+            }
+            else
+            {
+                myBackgroundWorker.CancelAsync();
+            }          
+        }
+
+        // init backgroundworker functions and attritibutes and other program status
+        public TextSearch()
+        {
+            InitializeComponent();
+            myBackgroundWorker.DoWork += new DoWorkEventHandler(myBackgroundWorker_DoWork);
+            myBackgroundWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(myBackgroundWorker_RunWorkerCompleted);
+            myBackgroundWorker.ProgressChanged += new ProgressChangedEventHandler(myBackgroundWorker_ProgressChanged);
+            myBackgroundWorker.WorkerReportsProgress = true;
+            myBackgroundWorker.WorkerSupportsCancellation = true;
+            SearchButton.Enabled = false;
+            FCStatusLabel.Text = "Ready.";
+        }
+
+        private void TextSearch_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // DoWork: read target file line by line and find out whether this line contains key word
+        // if it contains, send this line/s information to ReportProgress
+        protected void myBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        { 
+            String line;
+            StreamReader file = new StreamReader(FileName);
+            // continue reading while thie file is not finished
+            while ((line = file.ReadLine()) != null)
+            {
+                Thread.Sleep(1); // sleep 1 millisecond every time the program read a line
+                if (myBackgroundWorker.CancellationPending) // if it got a canael singal
+                {
+                    Cancelled = true;
+                    e.Cancel = true;
+                    break;
+                }
+                else if (line.ToLower().Contains(KeyWord.ToLower())) // case-insensitive, contains target key word
+                {
+                    findNumber++; // totally find findNumber lines
+                    LineNumber = count + 1; // target line number in real world
+                    myBackgroundWorker.ReportProgress(count, line); // send  line number and line context to ReportProgress
+                    Thread.Sleep(50); // sleep 50 millisecond after show one target line to make it looks better especially when there's lots of target lin ein one search
+                }
+                else
+                {
+                    myBackgroundWorker.ReportProgress(count);
+                }
+                count++;
+            }
+        }
+
+        //RunWorkerCompleted: reset parameters and progress bar
+        protected void myBackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // when the progerss is completed, set pregress bar depends on whether it's cancelled or finidhed
+            if(e.Cancelled)
+            {
+                FCStatusLabel.Text = "Cancelled.";
+                ProgressBar.Value = 0;
+            }
+            else
+            {
+                FCStatusLabel.Text = "Finished.";
+                ProgressBar.Value = totalline;
+            }
+            // reset counting paramaters
+            SearchButton.Text = "Search";
+            FileNameTextBox.ReadOnly = false;
+            SearchforTextBox.ReadOnly = false;
+            count = 0;
+            LineNumber = 0;
+            findNumber = 0;
+        }
+
+
+        // ProgressChanged: show the result on the interface
+        protected void myBackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            string f = e.UserState as String;
+            if (f == "" || f == null)
+            {
+
+            }
+            else
+            {
+                String[] row = { findNumber.ToString(), LineNumber.ToString(), f }; // result number, line number in txt file, line context
+                ListViewItem item = new ListViewItem(row);
+                ResultListView.Items.Add(item); // add this line's information to the list view
+                ResultListView.Items[ResultListView.Items.Count - 1].EnsureVisible(); // focus on the newest line
+            }            
+            ProgressBar.Maximum = totalline;
+            ProgressBar.Value = count;
+            FCStatusLabel.Text = "Searching..."; // program status is "Searching"
+            toolStripStatusLabel.Text = "Find " + (findNumber).ToString() + " lines contains target key word."; // show how many lines already found on status bar
+            StatusBar.Refresh();
+        }
+    }
+}
